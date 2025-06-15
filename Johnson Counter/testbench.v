59920fdb4a8efdad7f8b1e77ac0e38242631dfce
User-126 <146633221+User-126@users.noreply.github.com>
2025-06-15 12:37:29 +0530

    Create testbench.v

---
+++ Johnson Counter/testbench.v
@@ -0,0 +1,19 @@
+`timescale 1ns / 1ps
+
+
+module tb_johnson(
+
+    );
+    reg clk,mrst;
+    wire q0,q1,q2;
+    
+    johnson_top uut(.mrst(mrst),.clk(clk),.q0(q0),.q1(q1),.q2(q2));
+    always #5 clk = ~clk;
+    initial begin
+    clk = 0;
+     mrst = 0;  #11
+   mrst=1;
+    #200 $finish;
+    end
+    
+endmodule
